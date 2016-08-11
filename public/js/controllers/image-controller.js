61514a4ad72c4cce45873d3b841f3fd02c4d8acc
Rafael Viegas <rafael.viegas@riseupsolutions.com.br>
2016-08-11 11:08:29 -0300

    Melhorando a experiência do usuário

--- public/js/controllers/image-controller.js
+++ public/js/controllers/image-controller.js
@@ -1,7 +1,8 @@
 angular.module('alurapic').controller('ImageController', function($scope, $http){
 	
 	$scope.images = [];
-
+	$scope.search = '';
+	
 	$http.get('v1/fotos').success(function(result){
 		$scope.images = result;
 	}).error(function(error){
